a07f40e783d518757d597f222b75058a3ac951d2
Brandon LeBeau <lebebr01@gmail.com>
2018-01-09 13:45:50 -0600

    Update to start function definitions

--- R/Rcode_Data_v1.R
+++ R/Rcode_Data_v1.R
@@ -1,4 +1,4 @@
-  setwd("H:/BeckerAloe")
-  dat <- read.csv("Becker09.csv")
-  L1 <- vec2sm( as.matrix(dat[1,c(4:ncol(dat))]), diag = FALSE)
-    diag(L1) <- 1
+  # # setwd("H:/BeckerAloe")
+  # dat <- read.csv("Becker09.csv")
+  # L1 <- vec2sm( as.matrix(dat[1,c(4:ncol(dat))]), diag = FALSE)
+  #   diag(L1) <- 1
