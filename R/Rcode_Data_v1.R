fff81e61d3fdb7162eae1f77167a976cb18cd2e6
Brandon LeBeau <lebebr01@gmail.com>
2018-01-24 14:13:35 -0600

    Add prep_data function

--- R/Rcode_Data_v1.R
+++
@@ -1,4 +0,0 @@
-  # # setwd("H:/BeckerAloe")
-  # dat <- read.csv("Becker09.csv")
-  # L1 <- vec2sm( as.matrix(dat[1,c(4:ncol(dat))]), diag = FALSE)
-  #   diag(L1) <- 1
---
+++ R/prep_data.r
@@ -0,0 +1,45 @@
+#' Prep Data for Metafor
+#'
+#' @param data
+#' @param n Sample size
+#' @param type
+#' @param missing
+#' @param variable_names An optional character vector specifying variable names
+#'   of original data. This is mostly used to pass to \code{\link{df_to_corr}}.
+#'
+#' @importFrom Matrix bdiag
+#' @importFrom corpcor sm2vec
+#'
+#' @export
+prep_data <- function(data, n, type = c('average','weighted', 'simple'),
+                      missing = 'remove', variable_names = NULL) {
+
+  if(is.data.frame(data)) {
+    data <- df_to_corr(data, variables = variable_names)
+  }
+
+  olkin <- olkin_siotani(data, n, type)
+
+  variance_covariance <- var_cov(olkin)
+
+  data_vector_list <- lapply(data,  corpcor::sm2vec)
+  data_vector <-   matrix(unlist(data_vector_list))
+
+  outcome <- rep(1:length(data_vector_list[[1]]), length(data))
+  outcome <- as.factor(outcome)
+  study <- sort(rep(1:length(data), length(data_vector_list[[1]])))
+
+  if(missing == 'remove') {
+    miss_loc <- missing_data(data_vector)
+
+    data_vector <- data_vector[miss_loc]
+    variance_covariance <- variance_covariance[c(miss_loc), c(miss_loc)]
+    outcome <- outcome[miss_loc]
+    study <- study[miss_loc]
+  }
+
+  list(yi = data_vector,
+       V = variance_covariance,
+       outcome = outcome,
+       study = study)
+}
