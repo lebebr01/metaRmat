dbd9d2ca42f4476a68b9c0e62cf53a5f644ad43b
Brandon LeBeau <lebebr01@gmail.com>
2018-02-08 14:18:58 -0600

    Add extract_model function

--- R/util.r
+++ R/util.r
@@ -7,3 +7,15 @@ missing_data <- function(data) {
   -1 * which(is.na(data))
 
 }
+
+tau_matrix <- function(rho, tau2) {
+
+  rho_matrix <-  corpcor::vec2sm(rho)
+  diag(rho_matrix) <- 1
+
+  tau_mat <- corpcor::vec2sm(sqrt(tau2))
+  diag(tau_mat) <- 1
+
+  tau_mat %*% rho_matrix %*% tau_mat
+
+}
