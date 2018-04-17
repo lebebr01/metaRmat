cf7974bad42dadc8cac140318ff685d2b64523f3
Brandon LeBeau <lebebr01@gmail.com>
2018-04-17 18:02:08 -0500

    Summary functions working

--- R/util.r
+++ R/util.r
@@ -18,3 +18,13 @@ tau_matrix <- function(rho, tau2) {
   tau_mat %*% rho_matrix %*% tau_mat
 
 }
+
+# Identify and subset matrix
+matrix_subset <- function(matrix, variables) {
+
+  match_column <- colnames(matrix) %in% variables
+  match_row <- rownames(matrix) %in% variables
+
+  matrix[match_row, match_column]
+
+}
