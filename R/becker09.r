75608145f4993c53f8daa4072134b79192961091
Brandon <lebebr01@gmail.com>
2019-12-06 10:59:12 -0600

    Export data automatically

---
+++ R/becker09.r
@@ -0,0 +1,23 @@
+#' Becker 2009 data
+#'
+#' This data contains data originally collected by Becker in
+#'  2009 on ...
+#'
+#' @format A dataframe with 10 rows and 9 columns:
+#' \describe{
+#'    \item{ID}{Study ID}
+#'    \item{N}{Study sample size}
+#'    \item{Team}{...}
+#'    \item{Cognitive_Performance}{Correlation between Cognitive and Performance}
+#'    \item{Somatic_Performance}{Correlation between Somatic and Performance}
+#'    \item{Selfconfidence_Performance}{Correlation between Self Confidence and
+#'    Performance}
+#'    \item{Somatic_Cognitive}{Correlation between Somatic and Cognitive}
+#'    \item{Selfconfidence_Cognitive}{Correlation between Self Confidence and
+#'    Cognitive}
+#'    \item{Selfconfidence_Somatic}{Correlation between Self Confidence and
+#'    Somatic}
+#'    }
+#' @examples
+#'   becker09
+"becker09"
---
+++ R/math_correlations.r
@@ -0,0 +1,12 @@
+#' Math Correlations Data
+#'
+#' This data contains correlations on math ...
+#'
+#' @format A nested list with correlations and sample size:
+#' \describe{
+#'    \item{math_correlations}{A list of math correlations}
+#'    \item{sample_size}{Study sample size}
+#'    }
+#' @examples
+#'   math_correlations
+"math_correlations"
