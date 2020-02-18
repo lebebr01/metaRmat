896ad09eba072f440c111b08b28933c6d10484ad
Brandon <lebebr01@gmail.com>
2020-02-18 10:56:21 -0600

    Update jacobian function for special case

--- R/jacobian.r
+++ R/jacobian.r
@@ -7,32 +7,36 @@
 #' @export
 jacobian_stdslopes <- function(R) {
 
-  n <-ncol(R)
-  JacMat <- matrix(nrow=n-1, ncol=0.5*n*(n-1))
+  n <- ncol(R)
+  JacMat <- matrix(nrow = n - 1, ncol = 0.5 * n * (n - 1))
 
   #Partition of R for standardized slopes
-  R01 <- R[2:n,1]
-  R11 <- R[2:n,2:n]
+  R01 <- R[2:n, 1]
+  R11 <- R[2:n, 2:n]
 
   R11inv <- solve(R11)
 
-  for (i in 1:(n-1)) {
-    Ji <- as.vector(Matrix::sparseVector(1, i, n-1))
-    JacMat[,i] <- R11inv %*% Ji
+
+  for (i in 1:(n - 1)) {
+    Ji <- as.vector(Matrix::sparseVector(1, i, n - 1))
+    JacMat[, i] <- R11inv %*% Ji
   }
 
-  k <- n
+  if (n > 2){
+    k <- n
 
-  for (i in 1:(n-2)){
-    for (j in (i+1):(n-1)){
+    for (i in 1:(n - 2)){
+      for (j in (i + 1):(n - 1)){
 
-      Jij <- as.matrix(Matrix::sparseMatrix(c(i,j), c(j,i),
-                                    x=1, dims=c(n-1,n-1)))
-      JacMat[,k] <- -R11inv %*% Jij %*% R11inv %*% R01
+        Jij <- as.matrix(Matrix::sparseMatrix(c(i, j), c(j, i),
+                                              x=1, dims=c(n-1, n-1)))
+        JacMat[, k] <- -R11inv %*% Jij %*% R11inv %*% R01
 
-      k <- k+1
+        k <- k + 1
+      }
     }
   }
+
   JacMat
 }
 
